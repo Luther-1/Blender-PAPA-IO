21b09da7a610a736d85acc6e053229598421db90
Luther-1 <marcusder@hotmail.com>
2022-05-14 00:41:23 -0600

    Change copyright notice
    Raevn was incorrectly added

--- texture_extensions.py
+++ texture_extensions.py
@@ -1,6 +1,5 @@
 # The MIT License
 # 
-# Copyright (c) 2013, 2014  Raevn
 # Copyright (c) 2021, 2022  Marcus Der      marcusder@hotmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
