3f0b352e858eb2aaa64ad3b09146e7f07b99b686
Luther-1 <marcusder@hotmail.com>
2022-06-11 03:23:59 -0600

    Remove .cmd from git ignore

---
+++ build.cmd
@@ -0,0 +1,10 @@
+gcc -shared -O3 -c texture.c -o texture.o
+gcc -shared -o PTex.dll texture.o
+del texture.o
+
+gcc -O3 -fPIC -fopenmp -Wall -c -o texture.o texture_extensions.c
+gcc -shared -o ETex.dll texture.o -lgomp -static
+del texture.o
+
+rem gcc -O3 -fPIC -fopenmp -Wall -lgomp -g -o main.exe main.c
+pause
